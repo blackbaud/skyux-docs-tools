2623b9eec65d728f53e3971951abfaee1c8fd233
Bobby Earl <bobby.earl@blackbaud.com>
2019-11-18 17:44:58 -0500

    Made all properties optional.

--- src/app/public/modules/shared/docs-tools-component-info.ts
+++ src/app/public/modules/shared/docs-tools-component-info.ts
@@ -2,8 +2,8 @@
  * Represents the structure of information returned from the SKY Supportal's `component-info` endpoint.
  */
 export interface SkyDocsComponentInfo {
-  icon: string;
-  name: string;
-  summary: string;
-  url: string;
+  icon?: string;
+  name?: string;
+  summary?: string;
+  url?: string;
 }
