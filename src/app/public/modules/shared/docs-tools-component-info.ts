75b58387885a60901bf929706ef9159aa351363c
Alex Kingman <alex.kingman@blackbaud.me>
2020-07-07 16:22:21 -0400

    3.5.0 release

--- src/app/public/modules/shared/docs-tools-component-info.ts
+++ src/app/public/modules/shared/docs-tools-component-info.ts
@@ -5,7 +5,7 @@ export interface SkyDocsComponentInfo {
   children?: SkyDocsComponentInfo[];
   icon?: string;
   name?: string;
-  restricted?: string;
+  restricted?: boolean;
   summary?: string;
   thumbnail?: string;
   url?: string;
