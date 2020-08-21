45f22dd01cfeda02729d9e4e6f44d1acfbb763da
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-08-21 11:02:14 -0400

    Add support for constants

---
+++ src/app/public/modules/_documentation-test/foo-const.ts
@@ -0,0 +1,18 @@
+import {
+  FooEnum
+} from './foo-enum';
+
+/**
+ * This is the description for FOO_ARRAY.
+ */
+export const FOO_ARRAY = ['bar', 'baz'];
+
+/**
+ * This is the description for FOO_STRING.
+ */
+export const FOO_STRING = 'bar';
+
+/**
+ * This is the description for FOO_FUNC.
+ */
+export const FOO_FUNC = (foo: string, bar?: number) => FooEnum;
