790a389635723cf61aa98b4ccd7065ab9ff7e83a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-09-25 10:54:18 -0400

    Fix unit test

--- src/app/public/modules/type-definitions/directive-definition.component.spec.ts
+++ src/app/public/modules/type-definitions/directive-definition.component.spec.ts
@@ -113,8 +113,10 @@ describe('Directive definition component', function () {
 
     const rowElements = fixture.nativeElement.querySelectorAll('.sky-docs-property-definitions-table-cell-name');
 
-    expect(rowElements.item(0).innerText).toEqual('@Input()\nconfig?: Config');
-    expect(rowElements.item(1).innerText).toEqual('@Output()\nclick?: EventEmitter<string>');
+    expect(rowElements.item(0).innerText).toEqual(`@Input()
+config?: Config`);
+    expect(rowElements.item(1).innerText).toEqual(`@Output()
+click?: EventEmitter<string>`);
   });
 
   it('should display Output properties if Inputs do not exist', () => {
