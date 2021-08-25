1ea2f02e8f5e80560e6b0558d69d136f13f7c02a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-08-25 15:02:29 -0400

    Final attempt

--- scripts/after-build-public-library-success.js
+++ scripts/after-build-public-library-success.js
@@ -1,17 +1,7 @@
-const crossSpawn = require('cross-spawn');
-
 async function runAsync() {
   console.log('Preparing package for NPM...');
-  try {
-    crossSpawn.sync('node', ['./scripts/prepare-package.js'], {
-      cwd: process.cwd(),
-      stdio: 'inherit'
-    });
-    console.log('Done preparing package.');
-  } catch (err) {
-    console.error('Preparing package failed!');
-    process.exit(1);
-  }
+  require('./prepare-package');
+  console.log('Done preparing package.');
 }
 
 module.exports = {
