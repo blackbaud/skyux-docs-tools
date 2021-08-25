e1cccd802bd93ac83b041a0eda64f67e5fbd4cda
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-08-25 14:33:36 -0400

    Add logs

--- scripts/prepare-package.js
+++ scripts/prepare-package.js
@@ -4,16 +4,22 @@ const tildeImporter = require('node-sass-tilde-importer');
 const path = require('path');
 
 function copyScss() {
-  const result = sass.renderSync({
-    file: path.resolve(__dirname, '../src/app/public/styles/docs-tools.scss'),
-    importer: tildeImporter
-  });
+  console.log('Compiling docs-tools.css file...');
+  try {
+    const result = sass.renderSync({
+      file: path.resolve(__dirname, '../src/app/public/styles/docs-tools.scss'),
+      importer: tildeImporter
+    });
 
-  const target = path.resolve(__dirname, '../dist/css/docs-tools.css');
+    const target = path.resolve(__dirname, '../dist/css/docs-tools.css');
 
-  fs.ensureFileSync(target);
+    fs.ensureFileSync(target);
 
-  fs.writeFileSync(target, result.css);
+    fs.writeFileSync(target, result.css);
+  } catch (err) {
+    console.error('Compiling docs-tools.css failed!', err);
+    process.exit(1);
+  }
 }
 
 copyScss();
