6d0a3fd8369b16258f4a3bc11861ac1324b48e77
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-12-06 14:22:16 -0500

    Fix comment

--- projects/docs-tools/src/modules/code-examples/code-examples-editor.service.ts
+++ projects/docs-tools/src/modules/code-examples/code-examples-editor.service.ts
@@ -79,7 +79,7 @@ export class SkyDocsCodeExamplesEditorService {
     );
 
     // Ensure any @skyux dependencies list the correct version of SKY UX.
-    // e.g. `"@skyux/core": "*"` --> `"@skyux/core": "4.0.0"`
+    // e.g. `"@skyux/core": "*"` --> `"@skyux/core": "5.0.0"`
     for (const packageName in mergedDependencies) {
       /*istanbul ignore else*/
       if (mergedDependencies.hasOwnProperty(packageName)) {
