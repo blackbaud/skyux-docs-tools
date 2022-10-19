6b6c783cad344f736523e0f6ea1892f5c2866230
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-10-19 15:49:45 -0400

    Fix additional source code paths

--- projects/docs-tools/src/modules/demo-page/demo-page.component.spec.ts
+++ projects/docs-tools/src/modules/demo-page/demo-page.component.spec.ts
@@ -4,6 +4,8 @@ import { SkyMediaQueryService } from '@skyux/core';
 
 import { MockSkyMediaQueryService } from '@skyux/core/testing';
 
+import { expect } from '@skyux-sdk/testing';
+
 import { of } from 'rxjs';
 
 import { SkyDocsSupportalService } from '../shared/docs-tools-supportal.service';
@@ -13,7 +15,159 @@ import { SkyDocsTypeDefinitionsProvider } from '../type-definitions/type-definit
 import { DemoPageFixtureComponent } from './fixtures/demo-page.component.fixture';
 
 import { DemoPageFixturesModule } from './fixtures/demo-page-fixtures.module';
-
+import { SkyDocsTypeDefinitionsService } from '../type-definitions/type-definitions.service';
+import { MockTypeDocAdapterService } from '../type-definitions/fixtures/mock-type-definitions.service';
+import { SkyDocsTypeDocAdapterService } from '../type-definitions/typedoc-adapter.service';
+function getService(
+  provider: SkyDocsTypeDefinitionsProvider = {
+    anchorIds: {},
+    typeDefinitions: [
+      {
+        anchorId: '',
+        decorators: [{ name: 'Component', type: {} }],
+        name: 'FooComponent',
+        sources: [
+          {
+            fileName:
+              'src/app/public/modules/_documentation-test/foo.component.ts',
+          },
+        ],
+      },
+      {
+        anchorId: '',
+        decorators: [{ name: 'Directive', type: {} }],
+        name: 'FooDirective',
+        sources: [
+          {
+            fileName:
+              'src/app/public/modules/_documentation-test/foo.directive.ts',
+          },
+        ],
+      },
+      {
+        anchorId: '',
+        decorators: [{ name: 'Injectable', type: {} }],
+        name: 'FooService',
+        sources: [
+          {
+            fileName:
+              'src/app/public/modules/_documentation-test/foo.service.ts',
+          },
+        ],
+      },
+      {
+        anchorId: '',
+        decorators: [{ name: 'NgModule', type: {} }],
+        name: 'FooModule',
+        sources: [
+          {
+            fileName:
+              'src/app/public/modules/_documentation-test/foo.module.ts',
+          },
+        ],
+      },
+      {
+        anchorId: '',
+        decorators: [
+          {
+            name: 'Pipe',
+            type: {
+              declaration: {
+                children: [
+                  {
+                    name: 'transform',
+                    kindString: 'Method',
+                    signatures: [
+                      {
+                        name: 'transform',
+                        kindString: 'Call signature',
+                        parameters: [
+                          {
+                            name: 'value',
+                            kindString: 'Parameter',
+                            type: {
+                              type: 'reference',
+                              name: 'Date',
+                            },
+                          },
+                        ],
+                        type: {
+                          type: 'intrinsic',
+                          name: 'string',
+                        },
+                      },
+                    ],
+                  },
+                ],
+              },
+            },
+          },
+        ],
+        name: 'FooPipe',
+        sources: [
+          {
+            fileName: 'src/app/public/modules/_documentation-test/foo.pipe.ts',
+          },
+        ],
+      },
+      {
+        anchorId: '',
+        name: 'FooClass',
+        kindString: 'Class',
+        sources: [
+          {
+            fileName: 'src/app/public/modules/_documentation-test/foo-class.ts',
+          },
+        ],
+      },
+      {
+        anchorId: '',
+        name: 'Foo',
+        kindString: 'Interface',
+        sources: [
+          { fileName: 'src/app/public/modules/_documentation-test/foo.ts' },
+        ],
+      },
+      {
+        anchorId: '',
+        name: 'FooEnum',
+        kindString: 'Enumeration',
+        sources: [
+          {
+            fileName: 'src/app/public/modules/_documentation-test/foo-enum.ts',
+          },
+        ],
+      },
+      {
+        anchorId: '',
+        name: 'TypeAlias',
+        kindString: 'Type alias',
+        sources: [
+          {
+            fileName: 'src/app/public/modules/_documentation-test/foo-alias.ts',
+          },
+        ],
+      },
+      {
+        anchorId: '',
+        decorators: [{ name: 'Component', type: {} }],
+        name: 'BarComponent',
+        sources: [
+          {
+            fileName:
+              'src/app/public/modules/_documentation-test-additional/additional.component.ts',
+          },
+        ],
+      },
+    ],
+  }
+): SkyDocsTypeDefinitionsService {
+  const adapter = new MockTypeDocAdapterService();
+  return new SkyDocsTypeDefinitionsService(
+    provider,
+    adapter as SkyDocsTypeDocAdapterService
+  );
+}
 //#region helpers
 function getSidebarLinks(
   fixture: ComponentFixture<any>
@@ -35,7 +189,7 @@ describe('Demo page component', () => {
           provide: SkyMediaQueryService,
           useValue: mockMediaQueryService,
         },
-        SkyDocsTypeDefinitionsProvider,
+        { provide: SkyDocsTypeDefinitionsService, useValue: getService() },
       ],
     });
 
@@ -52,6 +206,40 @@ describe('Demo page component', () => {
     const pageTitle = document.title;
 
     expect(pageTitle).toEqual(component.pageTitle);
+  });
+
+  it('should show all items within the module source code', () => {
+    fixture.detectChanges();
+    const elements = document.querySelectorAll(
+      '.sky-docs-type-definition-wrapper sky-docs-heading-anchor'
+    );
+    expect(elements.length).toBe(8);
+    expect(elements[0]).toHaveText('FooComponent');
+    expect(elements[1]).toHaveText('FooDirective');
+    expect(elements[2]).toHaveText('FooPipe');
+    expect(elements[3]).toHaveText('FooService');
+    expect(elements[4]).toHaveText('FooClass');
+    expect(elements[5]).toHaveText('Foo');
+    expect(elements[6]).toHaveText('FooEnum');
+    expect(elements[7]).toHaveText('TypeAlias');
+  });
+
+  it('should show all items within the additional source code paths', () => {
+    fixture.componentInstance.additionalPaths = true;
+    fixture.detectChanges();
+    const elements = document.querySelectorAll(
+      '.sky-docs-type-definition-wrapper sky-docs-heading-anchor'
+    );
+    expect(elements.length).toBe(9);
+    expect(elements[0]).toHaveText('FooComponent');
+    expect(elements[1]).toHaveText('BarComponent');
+    expect(elements[2]).toHaveText('FooDirective');
+    expect(elements[3]).toHaveText('FooPipe');
+    expect(elements[4]).toHaveText('FooService');
+    expect(elements[5]).toHaveText('FooClass');
+    expect(elements[6]).toHaveText('Foo');
+    expect(elements[7]).toHaveText('FooEnum');
+    expect(elements[8]).toHaveText('TypeAlias');
   });
 
   it('should call the supportal service and update the sidebar routes', () => {
