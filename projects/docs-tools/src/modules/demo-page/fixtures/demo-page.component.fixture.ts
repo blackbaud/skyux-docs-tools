8786e54895ffed6ca117968ec55e96c9c05dc81f
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-10-19 16:27:32 -0400

    Additional source code paths fix (#225)

--- projects/docs-tools/src/modules/demo-page/fixtures/demo-page.component.fixture.ts
+++ projects/docs-tools/src/modules/demo-page/fixtures/demo-page.component.fixture.ts
@@ -5,5 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './demo-page.component.fixture.html',
 })
 export class DemoPageFixtureComponent {
+  public additionalPaths = false;
   public pageTitle = 'Sample docs';
 }
